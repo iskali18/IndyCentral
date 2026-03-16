c9f46bf3b8a2f7b646790a0c4d8fc5f57f81a609
iskali18 <iskali@yahoo.com>
2026-03-16 03:18:40 -0400

    Update event detail pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://indycentral.vercel.app",
-  trailingSlash: "always"
+  site: "https://indycentral.com",
+  trailingSlash: "always",
+  integrations: [sitemap()],
 });
